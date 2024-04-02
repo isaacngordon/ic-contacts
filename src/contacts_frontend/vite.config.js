b2a2fbfe895ded135b41635bbdba0dae5604a323
isaacngordon <45136480+isaacngordon@users.noreply.github.com>
2024-04-02 09:13:59 -0400

    Removed duplicate dependency and updated path alias in Vite configuration.

--- src/contacts_frontend/vite.config.js
+++ src/contacts_frontend/vite.config.js
@@ -43,19 +43,12 @@ export default defineConfig({
           new URL("../declarations", import.meta.url)
         ),
       },
+      {
+        find: "components",
+        replacement: fileURLToPath(
+          new URL("./src/components", import.meta.url)
+        ),
+      }
     ],
-  },
-});
-import { defineConfig } from 'vite';
-import reactRefresh from '@vitejs/plugin-react-refresh';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [reactRefresh()],
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-      '@declarations': '/src/declarations',
-    },
   },
 });
